4e152bd798b0bf6a4bd18878bc89e6c7cb932f1f
Indronil Chakroborty <chakraborty.indranil@gmail.com>
2023-03-30 16:19:40 +0530

    Replacing application.yml instead of application.properties file for database properties

--- src/main/java/com/mybookingshow/IdentityServiceApplication.java
+++ src/main/java/com/mybookingshow/IdentityServiceApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IdentityServiceApplication {
 
 	public static void main(String[] args) {
-		/*System.setProperty("mysql.driver-class-name", "com.mysql.cj.jdbc.Driver");
+		System.setProperty("mysql.driver-class-name", "com.mysql.cj.jdbc.Driver");
 		System.setProperty("mysql.service.username", "root");
 		System.setProperty("mysql.service.password", "admin");
-		System.setProperty("mysql.service.dialect", "org.hibernate.dialect.MySQL5Dialect");*/
+		System.setProperty("mysql.service.dialect", "org.hibernate.dialect.MySQL5Dialect");
 		SpringApplication.run(IdentityServiceApplication.class, args);
 
 	}
